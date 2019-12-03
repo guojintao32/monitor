7673f6de04fe0b7d3bf9953b94cefd58b82159b3
guojintao <15862418120@163.com>
2019-12-03 16:56:21 +0800

    修改client server

--- acceptor/client/server.js
+++ acceptor/client/server.js
@@ -1,6 +1,18 @@
 const Koa = require('koa');
 const app = new Koa();
-const static = require('koa-static');
+const path = require('path');
+const fs = require('fs');
+const router = require('koa-router')();
 
-app.use(static('./dist'));
+router.get('*', async (ctx, next) => {
+  if(ctx.req.url === '/') {
+    ctx.req.url = 'index.html';
+    ctx.type='text/html;charset=utf-8'
+  }
+  //ctx.req.url = ctx.req.url.replace('/','');
+  console.log(path.join(__dirname,'./dist',ctx.req.url))
+  ctx.response.body = fs.readFileSync(path.join(__dirname,'./dist',ctx.req.url))
+})
+
+app.use(router.routes());
 app.listen('8082');
